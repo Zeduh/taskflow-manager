e4daa484cdb45e00f11bdc0575edb0620c988e1f
José Eduardo <jeduardocury@gmail.com>
2025-02-07 00:51:17 -0300

    feat: implementar autenticação com login e registro de usuários

---
+++ frontend/app/validations/auth.validation.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const registerSchema = z.object({
+  name: z.string().min(3, 'Nome deve ter pelo menos 3 caracteres'),
+  email: z.string().email('Email inválido'),
+  password: z.string().min(6, 'Senha deve ter pelo menos 6 caracteres'),
+  confirmPassword: z.string()
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "As senhas não coincidem",
+  path: ["confirmPassword"],
+});
